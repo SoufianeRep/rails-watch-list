847fd4fe232cc934bc5bb10e5f458a4c8eb28f72
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-08-15 09:16:33 +0900

    added reviews styles

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'lists#index'
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :reviews, only: [:create]
     resources :bookmarks, only: [:create]
   end
